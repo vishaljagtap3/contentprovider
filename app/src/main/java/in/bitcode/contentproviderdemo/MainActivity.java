f3cd1477e8cf6e6e7a4f3e33143f27a1ecb12694
vishaljagtap3 <vishal.jagtap@bitcode.in>
2020-12-14 22:20:07 +0530

    Content Provider

--- app/src/main/java/in/bitcode/contentproviderdemo/MainActivity.java
+++ app/src/main/java/in/bitcode/contentproviderdemo/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CallLog;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,12 +19,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ContentResolver resolver = getContentResolver();
+        /*
+        Cursor c = resolver.query(CallLog.Calls.CONTENT_URI, null, null, null, null);
+
+        mt("----------------------------");
+        for(String colName : c.getColumnNames()) {
+                mt("col: " + colName);
+        }
+        mt("----------------------------");
+
+
+        int colNumber = c.getColumnIndex(CallLog.Calls.NUMBER);
+        int colName = c.getColumnIndex(CallLog.Calls.CACHED_NAME);
+        int colType = c.getColumnIndex(CallLog.Calls.TYPE);
+        int colDuration = c.getColumnIndex(CallLog.Calls.DURATION);
+
+        while(c.moveToNext()) {
+            mt( c.getString(colNumber) + " -> " + c.getString(colName) + " -> " + c.getInt(colType) + " -> " + c.getLong(colDuration));
+        }
+        */
+
+        Uri uri = Uri.parse("content://sms/");
+        Cursor c = resolver.query(uri, null, null, null, null);
+        mt("----------------------------");
+        for(String colName : c.getColumnNames()) {
+            mt("col: " + colName);
+        }
+        mt("----------------------------");
+
+        int colThreadId = c.getColumnIndex("thread_id");
+        int colType = c.getColumnIndex("type");
+        int colDate = c.getColumnIndex("date");
+        int colAddress = c.getColumnIndex("address");
+        int colBody = c.getColumnIndex("body");
+
+        while( c.moveToNext() ) {
+            mt( c.getLong(colThreadId ) + "\n" + c.getInt(colType) + "\n" + c.getString(colDate) + "\n" + c.getString(colAddress) + "\n" + c.getString(colBody));
+        }
+
+
+
+
+
+        /*
         Uri uri = Uri.parse("content://in.bitcode.students");
         uri = Uri.withAppendedPath(uri, "students");
-        //uri = Uri.withAppendedPath(uri, "11");
 
-        ContentResolver resolver = getContentResolver();
+        ContentValues values = new ContentValues();
+        values.put("id", 66);
+        values.put("name", "FF");
+        values.put("marks", 90);
 
+        uri = resolver.insert(uri, values);
+        Log.e("tag", "Got Uri: " + uri);
+        */
+        //content://in.bitcode.storage/students/66
+
+
+
+        //uri = Uri.withAppendedPath(uri, "33");
+
+        /*
         Cursor c = resolver.query(
                 uri,
                 null,
@@ -30,6 +88,8 @@ public class MainActivity extends AppCompatActivity {
                 null,
                 null
         );
+        */
+
         /*
         Cursor c = resolver.query(
                 uri,
@@ -41,15 +101,18 @@ public class MainActivity extends AppCompatActivity {
         */
 
 
+        /*
         String [] columnNames = c.getColumnNames();
-
         while(c.moveToNext()) {
             mt(c.getInt(0) + " " + c.getString(1) +  " " + c.getInt(2));
         }
+        */
 
+        /*
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType("image/*");
         startActivityForResult(intent, 1);
+         */
     }
 
     @Override
